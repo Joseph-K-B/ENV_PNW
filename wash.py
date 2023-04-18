9c38a1835b00a928832118a194eaed51fa7ac998
Joseph <pdxjosephb@gmail.com>
2023-04-17 19:20:31 -0700

    updated wash, added USGS data

--- wash.py
+++ wash.py
@@ -1,56 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("./dt_temp_1.csv")
+# Read in the data
+at_df = pd.read_csv("./data/at.csv")
+wt_df = pd.read_csv("./data/wt_data.csv")
 
-df["S"] = df["YEAR"].str.split("-").map(lambda items : sorted(items, key=str))
+wt_df = wt_df.drop([ "agency_cd", "site_no", "parameter_cd", "ts_id"], 1).rename(columns={"year_nu": "Year", "mean_va": "Water_Temp_C", "month_nu": "Month"})
 
-month = []
-year = []
-
-for i, row in df.iterrows():
-  month.append(row["S"][0])
-  year.append(row["S"][1])
-
-df["month"] = month
-df["year"] = year
-df = df.drop(["YEAR", "S"], 1)
-
-jan = df[df["month"] == "01"]
-jan.to_csv("jan.csv")
-
-feb = df[df["month"] == "02"]
-feb.to_csv("feb.csv")
-
-march = df[df["month"] == "03"]
-march.to_csv("march.csv")
-
-april = df[df["month"] == "04"]
-april.to_csv("april.csv")
-
-may = df[df["month"] == "05"]
-may.to_csv("may.csv")
-
-june = df[df["month"] == "06"]
-june.to_csv("june.csv")
-
-july = df[df["month"] == "07"]
-july.to_csv("july.csv")
-
-august = df[df["month"] == "08"]
-august.to_csv("august.csv")
-
-september = df[df["month"] == "09"]
-september.to_csv("september.csv")
-
-october = df[df["month"] == "10"]
-october.to_csv("october.csv")
-
-november = df[df["month"] == "11"]
-november.to_csv("november.csv")
-
-december = df[df["month"] == "12"]
-december.to_csv("december.csv")
-
-# df.plot(x='YEAR', y='TEMP', kind='line')
-# plt.show()
+df = pd.merge(at_df, wt_df, on="Year", how="outer").set_index("Year").dropna().drop("Month_x", 1).rename(columns={"Month_y": "Month"})
+df.to_csv("./data/wash.csv")
